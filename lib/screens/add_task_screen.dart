901e05ab095cc74330242fc7069f42cd1116f380
coltcarson <colt_carson@live.com>
2019-08-09 16:10:36 -0400

    Initial commit

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFF757575),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20.0),
+            topLeft: Radius.circular(20.0),
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                'Add Task',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.lightBlueAccent,
+                  fontSize: 30.0,
+                ),
+              ),
+              TextField(
+                autofocus: true,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              FlatButton(
+                child: Text(
+                  'Add',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.lightBlueAccent,
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
