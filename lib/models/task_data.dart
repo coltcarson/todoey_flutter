dadd2f12296a2ee574b7f7a5a9eeae8d819e5798
coltcarson <colt_carson@live.com>
2019-08-15 10:19:30 -0400

    Completed Provider Package

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/foundation.dart';
+import 'dart:collection';
 
 import '../models/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Buy Milk'),
     Task(name: 'Buy Eggs'),
     Task(name: 'Buy Bread'),
   ];
 
+  // Must use UnmodifiableList so we cannot edit lists from the outside
+  // but can still call off it i.e., Copy.
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
+  }
+
+  // Must always use a method to addTask to the list so we can call NotifyListeners()
+  void addTask(String newTaskTitle) {
+    final task = Task(name: newTaskTitle);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
   }
 }
